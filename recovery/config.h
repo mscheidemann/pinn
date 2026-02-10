e9ed00ee12b10e398e8d2f037b40225f8ec1bbc0
procount <kevin.procount@googlemail.com>
2026-02-10 00:27:56 +0000

    p3.9.8

--- recovery/config.h
+++ recovery/config.h
@@ -3,7 +3,7 @@
 
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "3.9.7c"
+#define VERSION_NUMBER "3.9.8"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
